c7acd5cdc036836da0bd548b195f5e54a06307c3
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2022-01-09 18:52:41 -0500

    setup completed

---
+++ TaskManager/Shared/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  Shared
+//
+//  Created by Rupesh Chhetri on 1/9/22.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
